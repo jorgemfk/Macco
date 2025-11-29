0f9411b5009ae6558984c6a96e992d9353e3b0e0
Jorge Luis Martinez Ramirez <jmartine@procesar.com>
2025-11-28 23:05:31 -0600

    olfato

---
+++ Olfato/olfato_esp32/olfato_esp32.ino
@@ -0,0 +1,88 @@
+#include <Wire.h>
+#include <U8g2lib.h>
+
+#define MQ3_PIN 34
+#define CALIBRACIONES 200   // muestras para calibrar
+
+U8G2_SSD1309_128X64_NONAME0_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+float filtro = 0;
+float base = 0;   // valor de MQ-3 en aire limpio
+
+void calibrarSensor() {
+  Serial.println("Calibrando MQ-3 (5 segundos)...");
+  long suma = 0;
+
+  for (int i = 0; i < CALIBRACIONES; i++) {
+    suma += analogRead(MQ3_PIN);
+    delay(25);
+  }
+
+  base = suma / CALIBRACIONES;
+  Serial.print("Base calibrada = ");
+  Serial.println(base);
+}
+
+float leerAlcoholPct() {
+  int raw = analogRead(MQ3_PIN);
+  Serial.println(raw);
+
+  filtro = 0.90 * filtro + 0.10 * raw;
+
+  float pct = (filtro - base) / (4095.0 - base) * 100.0;
+
+  if (pct < 0) pct = 0;
+  if (pct > 100) pct = 100;
+
+  return pct;
+}
+
+float estimarMgL(float pct) {
+  
+  return pct * 0.006; // mg/L aproximado
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Wire.begin(21, 22);
+  u8g2.begin();
+  u8g2.setContrast(255);
+
+  calibrarSensor(); // <-- NUEVO
+}
+
+void loop() {
+  float pct = leerAlcoholPct();
+  float mgL = estimarMgL(pct);
+  String estado = (mgL > 0.039) ? "NO CONDUCIR" : "PERMITIDO";
+
+  // OLED
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x12_tf);
+  u8g2.drawStr(0, 10, "Medidor Alcohol MQ3");
+
+  char l1[32];
+  sprintf(l1, "Alcohol: %.1f %%", pct);
+  u8g2.drawStr(0, 25, l1);
+
+  char l2[32];
+  sprintf(l2, "Aire: %.3f mg/L", mgL);
+  u8g2.drawStr(0, 40, l2);
+
+  u8g2.setFont(u8g2_font_7x14B_tf);
+  u8g2.drawStr(0, 60, estado.c_str());
+  u8g2.sendBuffer();
+
+  // Serial Debug
+  Serial.print("RAW filtrado: ");
+  Serial.print(filtro);
+  Serial.print(" | Base: ");
+  Serial.print(base);
+  Serial.print(" | % alcohol: ");
+  Serial.print(pct);
+  Serial.print(" | mg/L: ");
+  Serial.println(mgL);
+
+  delay(200);
+}
