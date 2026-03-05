d06f0ceef1834c85923678d4300f73805e4f699d
Jorge Luis Martinez Ramirez <jmartine@procesar.com>
2026-03-05 08:10:47 -0600

    version I2C

--- Oido/baldosas_sonido.py
+++ Oido/baldosas_sonido.py
@@ -1,126 +1,97 @@
 from Maix import MIC_ARRAY as mic
-from fpioa_manager import fm
-from machine import PWM, Timer
-from Maix import GPIO
-from board import board_info
-import sensor, image, lcd, time
+import lcd
+from machine import I2C
+import time
+import math
+import ustruct
 
-# CONFIG
-MAX_ANGULO = 100
-TIEMPO_ACTIVO_MS = 1 * 20 * 1000  # 5 minutos en milisegundos
-
-# LED reloj mapeado a ejes: (X, -X, Y, -Y)
-led_ejes = [
-    (1.0, 0.0, 0.0, 0.0), (0.7, 0.0, 0.7, 0.0), (0.0, 0.0, 1.0, 0.0),
-    (0.0, 0.0, 0.7, 0.7), (0.0, 0.7, 0.0, 0.7), (0.0, 1.0, 0.0, 0.0),
-    (0.0, 1.0, 0.0, 0.0), (0.0, 0.7, 0.0, 0.7), (0.0, 0.0, 0.7, 0.7),
-    (0.0, 0.0, 1.0, 0.0), (0.7, 0.0, 0.7, 0.0), (1.0, 0.0, 0.0, 0.0),
-]
-
-servo_pins = {
-    'X':   17,
-    '-X':  35,
-    'Y':   34,
-    '-Y':  33,
-}
-
-colores = {
-    'X':   (255, 0, 0),
-    '-X':  (0, 255, 0),
-    'Y':   (0, 0, 255),
-    '-Y':  (255, 255, 0),
-}
-
-# REGISTRO DE PINES
-fm.register(17, fm.fpioa.GPIO0)
-fm.register(35, fm.fpioa.GPIO1)
-fm.register(34, fm.fpioa.GPIO2)
-fm.register(33, fm.fpioa.GPIO3)
-
-# INICIALIZACIÓN
 lcd.init()
 mic.init()
-sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time=2000)
 
-# SERVOS
-servo_pwms = {
-    'X':   PWM(Timer(Timer.TIMER0, Timer.CHANNEL0, mode=Timer.MODE_PWM), freq=50, duty=0, pin=17),
-    '-X':  PWM(Timer(Timer.TIMER1, Timer.CHANNEL0, mode=Timer.MODE_PWM), freq=50, duty=0, pin=35),
-    'Y':   PWM(Timer(Timer.TIMER2, Timer.CHANNEL0, mode=Timer.MODE_PWM), freq=50, duty=0, pin=34),
-    '-Y':  PWM(Timer(Timer.TIMER0, Timer.CHANNEL1, mode=Timer.MODE_PWM), freq=50, duty=0, pin=33),
-}
-estado_servo = {eje: 0 for eje in servo_pins.keys()}
+# ---------------- PCA9685 ----------------
 
-# FUNCIONES
+class PCA9685:
+    def __init__(self, i2c, address=0x40):
+        self.i2c = i2c
+        self.address = address
+        self.reset()
 
-def mover_servo(pwm, grados):
-    grados = max(0, min(grados, MAX_ANGULO))
-    pulse = 500 + int((grados / MAX_ANGULO) * 2000)
-    duty = int((pulse / 20000.0) * 100)
-    pwm.duty(duty)
+    def _write(self, address, value):
+        self.i2c.writeto_mem(self.address, address, bytearray([value]))
 
-def calcular_angulos_direccion(leds):
-    pesos = {'X': 0, '-X': 0, 'Y': 0, '-Y': 0}
-    for i, intensidad in enumerate(leds):
-        px, nx, py, ny = led_ejes[i]
-        pesos['X'] += intensidad * px
-        pesos['-X'] += intensidad * nx
-        pesos['Y'] += intensidad * py
-        pesos['-Y'] += intensidad * ny
-    max_val = max(pesos.values())
-    if max_val == 0:
-        return {k: 0 for k in pesos}
-    for k in pesos:
-        pesos[k] = int((pesos[k] / max_val) * MAX_ANGULO)
-    return pesos
+    def _read(self, address):
+        return self.i2c.readfrom_mem(self.address, address, 1)[0]
 
-# DETECCIÓN DE MOVIMIENTO
-ref_img = sensor.snapshot()
-ultimo_movimiento = 0
-activo = False
+    def reset(self):
+        self._write(0x00, 0x00)
 
-# LOOP PRINCIPAL
+    def freq(self, freq=None):
+        prescale = int(25000000.0 / 4096.0 / freq + 0.5)
+        old_mode = self._read(0x00)
+        self._write(0x00, (old_mode & 0x7F) | 0x10)
+        self._write(0xfe, prescale)
+        self._write(0x00, old_mode)
+        time.sleep_us(5)
+        self._write(0x00, old_mode | 0xa1)
+
+    def pwm(self, index, on=None, off=None):
+        if on is None or off is None:
+            data = self.i2c.readfrom_mem(self.address, 0x06 + 4 * index, 4)
+            return ustruct.unpack('<HH', data)
+        data = ustruct.pack('<HH', on, off)
+        self.i2c.writeto_mem(self.address, 0x06 + 4 * index, data)
+
+    def duty(self, index, value=None):
+        if value is None:
+            pwm = self.pwm(index)
+            return pwm[1]
+        self.pwm(index, 0, value)
+
+class Servos:
+    def __init__(self, i2c, address=0x40, freq=50, min_us=500, max_us=2500, degrees=180):
+        self.period = 1000000 / freq
+        self.min_duty = int(4095 * min_us / self.period)
+        self.max_duty = int(4095 * max_us / self.period)
+        self.degrees = degrees
+        self.pca9685 = PCA9685(i2c, address)
+        self.pca9685.freq(freq)
+
+    def position(self, index, degrees):
+        span = self.max_duty - self.min_duty
+        duty = self.min_duty + span * degrees / self.degrees
+        duty = min(self.max_duty, max(self.min_duty, int(duty)))
+        self.pca9685.duty(index, duty)
+
+# ---------------- I2C y servos ----------------
+
+i2c1 = I2C(I2C.I2C0, mode=I2C.MODE_MASTER, scl=7, sda=6)
+s = Servos(i2c1)
+
+# ---------------- LOOP PRINCIPAL ----------------
+
 while True:
+
     imga = mic.get_map()
-    leds = mic.get_dir(imga)
-    img = sensor.snapshot()
-    hist = img.get_histogram()
-    stats = hist.get_statistics()
-    #ref_img = img.copy()
-    print(leds)
-    # Movimiento si varianza supera un umbral (ajustable)
-    if stats.mean() > 40: #stats.stdev() > 15:
-        print("Movimiento detectado")
-        ultimo_movimiento = time.ticks_ms()
-        activo = True
-    elif time.ticks_diff(time.ticks_ms(), ultimo_movimiento) > TIEMPO_ACTIVO_MS:
-        activo = False
+    sound = mic.get_dir(imga)
 
-    if activo:
-        grados = calcular_angulos_direccion(leds)
+    print(sound)
 
-        for eje in grados:
-            mover_servo(servo_pwms[eje], grados[eje])
-            estado_servo[eje] = grados[eje]
-    else:
-        grados = {k: 0 for k in servo_pins}
-        for eje in grados:
-            mover_servo(servo_pwms[eje], 0)
-            estado_servo[eje] = 0
+    # visualización
+    mic.set_led(sound, (0,0,255))
+    imgb = imga.resize(160,160)
+    imgc = imgb.to_rainbow(1)
+    lcd.display(imgc)
 
-    # LED combinando color
-    r, g, b = 0, 0, 0
-    for eje in grados:
-        if grados[eje] > 10:
-            cr, cg, cb = colores[eje]
-            r += cr
-            g += cg
-            b += cb
-    mic.set_led(leds, (min(r, 255), min(g, 255), min(b, 255)))
+    # controlar servos
+    for servo in range(6):
 
-    # Mostrar visualización de mic
-    lcd.display(imga.resize(160, 160).to_rainbow(1))
-    time.sleep(0.5)
+        i1 = servo * 2
+        i2 = servo * 2 + 1
+
+        intensidad = sound[i1] + sound[i2]
+
+        angulo = 120 * intensidad / 30
+
+        s.position(servo, angulo)
+
+    time.sleep_ms(50)
